af07af81ef037718fc20f7769e5c8b4551bdbb1c
TheK0tYaRa <alod123@mail.ua>
2021-02-04 03:53:15 +0200

    A

---
+++ musical-bot/target/rls/debug/deps/musical_bot-37d8cae927da4fa6.d
@@ -0,0 +1,5 @@
+/home/thek0tyara/Documents/VSCodeProjects/Musical_bot/musical-bot/target/rls/debug/deps/musical_bot-37d8cae927da4fa6.rmeta: src/main.rs
+
+/home/thek0tyara/Documents/VSCodeProjects/Musical_bot/musical-bot/target/rls/debug/deps/musical_bot-37d8cae927da4fa6.d: src/main.rs
+
+src/main.rs:
---
+++ musical-bot/target/rls/debug/deps/musical_bot-57405cbaa8e29316.d
@@ -0,0 +1,5 @@
+/home/thek0tyara/Documents/VSCodeProjects/Musical_bot/musical-bot/target/rls/debug/deps/musical_bot-57405cbaa8e29316.rmeta: src/main.rs
+
+/home/thek0tyara/Documents/VSCodeProjects/Musical_bot/musical-bot/target/rls/debug/deps/musical_bot-57405cbaa8e29316.d: src/main.rs
+
+src/main.rs:
